d22987c53cb20e13b1fd248806b61d690ff1d8b2
ankit detroja <ankit.b.detroja@gmail.com>
2021-12-07 04:41:15 +0530

    play

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 const score = document.querySelector(".score");
 const startScreen = document.querySelector(".startScreen");
 const gameArea = document.querySelector(".gameArea");
-
+const selmon = document.querySelector(".selmon");
+const code = document.querySelector('.code');
 let player = {
   speed: 5,
-  x: 0,
-  y: 0,
-  start: false,
+  score: 0,
 };
 
 let keys = {
@@ -20,24 +19,81 @@ startScreen.addEventListener("click", start);
 document.addEventListener("keydown", pressOn);
 document.addEventListener("keyup", pressOff);
 
-function playgame() {
-  console.log("inplay");
-  if (player.start) {
-    if (keys.ArrowUp) {
-      player.y += player.speed;
+function moveLines() {
+  let lines = document.querySelectorAll(".line");
+  lines.forEach(function (item) {
+    if (item.y > 1050) {
+      item.y = item.y - 1200;
     }
-    if (keys.ArrowDown) {
+    item.y = item.y + player.speed;
+    item.style.top = item.y + "px";
+  });
+}
+
+function isCollide(a, b) {
+  let aRect = a.getBoundingClientRect();
+  let bRect = b.getBoundingClientRect();
+
+  return !(
+    (aRect.bottom < bRect.top) || (aRect.top > bRect.bottom) || (aRect.left > bRect.right) || (aRect.right < bRect.left)
+  )
+
+}
+
+function endGame() {
+  player.start = false;
+  score.innerHTML = "Game Over <br> Score: " + player.score;
+  selmon.classList.remove('hide');
+  startScreen.classList.remove('hide');
+  startScreen.innerText = 'RESTART';
+  startScreen.style.color = 'white';
+}
+
+function moveEnemy(car) {
+  let ele = document.querySelectorAll(".enemy");
+  ele.forEach(function (item) {
+    if (isCollide(car, item)) {
+      console.log('Selmon Bhoi is Here!');
+      endGame();
+    }
+    if (item.y > 1050) {
+      item.y = item.y - 1200;
+      item.style.left = Math.floor(Math.random() * 450) + 'px';
+    }
+    item.y = item.y + player.speed;
+    item.style.top = item.y + "px";
+  });
+}
+
+function playgame() {
+  let car = document.querySelector(".car");
+  moveLines();
+  moveEnemy(car);
+  let road = gameArea;
+  if (player.start) {
+    if (keys.ArrowUp && car.offsetTop > 510) {
       player.y -= player.speed;
     }
-    if (keys.ArrowRight) {
+    if (keys.ArrowDown && car.offsetTop < 830) {
+      player.y += player.speed;
+    }
+    if (keys.ArrowRight && car.offsetLeft < 446) {
       player.x += player.speed;
     }
-    if (keys.ArrowLeft) {
+    if (keys.ArrowLeft && car.offsetLeft > 0) {
       player.x -= player.speed;
     }
-    car.style.left = player.x;
-    car.style.top = player.y;
+    car.style.left = player.x + "px";
+    car.style.top = player.y + "px";
     window.requestAnimationFrame(playgame);
+    player.score++;
+    score.innerText = "Score: " + player.score;
+    score.style.top = 30 + 'px';
+    score.style.left = '50px';
+    score.style.padding = '20px 50px';
+    score.style.boxShadow = "rgba(0, 0, 0, 0.05) 0px 6px 24px 0px,rgba(0, 0, 0, 0.08) 0px 0px 0px 1px";
+
+
   }
 }
 
@@ -53,17 +109,36 @@ function pressOff(e) {
   // console.log(keys);
 }
 
-let car = document.createElement("div");
 function start() {
   // console.log("clicked");
   startScreen.classList.add("hide");
   gameArea.classList.remove("hide");
+  selmon.classList.add('hide');
+  code.classList.add('hide');
+  gameArea.innerHTML = '';
   player.start = true;
+  player.score = 0;
+  for (let x = 0; x < 10; x++) {
+    let div = document.createElement("div");
+    div.classList.add("line");
+    div.y = x * 150;
+    div.style.top = (x * 150) + "px";
+    gameArea.appendChild(div);
+  }
+  let car = document.createElement("div");
   car.setAttribute("class", "car");
-  car.innerText = "Car";
+  // car.innerText = "Car";
   gameArea.appendChild(car);
   player.x = car.offsetLeft;
   player.y = car.offsetTop;
-  console.log(player);
   window.requestAnimationFrame(playgame);
+
+  for (let x = 0; x < 6; x++) {
+    let enemy = document.createElement("div");
+    enemy.classList.add("enemy");
+    enemy.y = ((x + 1) * 1000) * -1;
+    enemy.style.left = Math.floor(Math.random() * 450) + 'px';
+    enemy.style.top = enemy.y + "px";
+    gameArea.appendChild(enemy);
+  }
 }
